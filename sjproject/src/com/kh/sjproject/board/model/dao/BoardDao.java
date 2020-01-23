447b5c034d8862729799a20bbe6c5776c3e390fd
user1 <user1@user1-PC>
2020-01-23 17:53:23 +0900

    sdfsadf

--- sjproject/src/com/kh/sjproject/board/model/dao/BoardDao.java
+++ sjproject/src/com/kh/sjproject/board/model/dao/BoardDao.java
@@ -18,7 +18,7 @@ import com.kh.sjproject.board.model.vo.Reply;
 public class BoardDao {
 	private Properties prop = null;
 
-	public BoardDao() throws Exception{
+	public BoardDao() throws Exception{  
 		String fileName =
 				BoardDao.class
 				.getResource("/com/kh/sjproject/sql/board/board-query.properties").getPath();
